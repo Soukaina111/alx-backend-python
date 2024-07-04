51322dd4520bfa1860bd9302aa0567b81b39c11f
Soukaina111 <basmahajar@gmail.com>
2024-07-04 15:23:19 +0000

    2-3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+floor Annotation
+"""
+
+
+def floor(n: float) -> int:
+    """
+    floor of the number_function
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+to string-Annotation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    float to string Conversion
+    """
+    return str(n)
