3273c6f76604169c0361a478882be9f4b3b240cc
Soukaina111 <basmahajar@gmail.com>
2024-07-04 15:19:18 +0000

    2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Concatenation-Annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function to concats 2 strs
+    """
+    return str1 + str2
